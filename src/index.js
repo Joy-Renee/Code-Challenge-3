027a702cd83e85031d34cadb147d1271b7676701
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-08 14:46:03 +0300

    initial Commit

--- src/index.js
+++ src/index.js
@@ -32,9 +32,13 @@ const forTitles = function() {
     .then(data => {
         console.log(data);
         data.forEach(movie => {
-            const titleDiv = document.createElement('div')
-            titleDiv.textContent = movie.title
-            titleFilms.appendChild(titleDiv)
+            //console.log(movie);
+            const titleList = document.createElement('li')
+            titleList.innerHTML = `
+            <button id='${movie.id}' onclick ="movieDetails()" > ${movie.title}</button>
+            `
+            titleFilms.appendChild(titleList)
+
 
         })
     })
@@ -42,9 +46,42 @@ const forTitles = function() {
 }
 forTitles()
 
-console.log( document.querySelector('#f-Items').addEventListener('mousover', forTitles))
+function movieDetails(){
+    fetch("http://localhost:3000/films")
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data);
+        data.forEach(movie => {
+            const button = document.getElementById(`${movie.id}`)
+            button.addEventListener('click', e =>{
+                let cards = document.getElementById('showing')
+            let remainingTicket = `${movie.capacity}` - `${movie.tickets_sold}`
+            cards.innerHTML = `
+            <div class= 'card'> </div>
+            <div id= 'title' class = 'title'> ${movie.title} </div>
+            <div id= 'runtime' class = 'meta'>${movie.runtime} minutes </div>
+            <div class="content">
+             <div class="description">
+                <div id = 'film-info'>${movie.description}</div>
+                <span id="showtime" class="ui label">${movie.showtime}</span>
+                <span id="ticket-num">${remainingTicket}</span> remaining tickets
+            </div>
+            div class="extra content">
+              <button id="buy-ticket" class="ui orange button">
+                Buy Ticket
+              </button>
+            </div>
+            `  
+        const pic = document.getElementById('poster')   
+        pic.src =`${movie.poster}`   
+            })
+        })
+        })
+}
 
+// console.log( document.querySelector('#f-Items').addEventListener('mousover', forTitles))
 
+//function clickEvent
 
 /*
 const forPoster = function(){
