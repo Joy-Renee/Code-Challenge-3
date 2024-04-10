a36d99b9d700e0a6403505a75a9a99cbcc50ae54
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-10 18:19:11 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,31 +1,8 @@
 // Your code here
-/* console.log(document.querySelectorAll('ul#films'))
 
-const list = document.getElementById("f-Items")
-
-function movieTitles(){
-    let titles = document.getElementsByClassName('.film item')
-   
-}
-console.log(movieTitles);
-
-function getTitles(fi){
-    fetch('http://localhost:3000/films')
-    .then(res => res.json() )
-    .then(films => films.forEach(film => movieTitles(film))
-    )
-}
-
-function initialize(){
-    return console.log(getTitles())
-}
-
-console.log(initialize)
-
-*/
 
 const link = "https://json-server-1-wvrp.onrender.com/films"
-console.log(link);
+// console.log(link);
 const titleFilms = document.getElementById("f-Item")
 
 const forTitles = function() {
@@ -35,10 +12,12 @@ const forTitles = function() {
         console.log(data);
         data.forEach(movie => {
             //console.log(movie);
-            const titleList = document.createElement('li')
+            const titleList = document.createElement('ul')
             titleList.innerHTML = `
-            <button id='${movie.id}' onclick ="movieDetails()" > ${movie.title}</button>
+            <ul class = 'ul-btn 'id='${movie.id}' onclick ="movieDetails()" > ${movie.title}</ul>
+            <button id = 'delete-btn'> Delete </button> 
             `
+            // ul-btn.style.backgroundColor = 'yellow'
             titleFilms.appendChild(titleList)
 
 
@@ -48,6 +27,8 @@ const forTitles = function() {
 }
 forTitles()
 
+
+// This function is being used to add the movie list and its details including the image
 function movieDetails(){
     fetch( "https://json-server-1-wvrp.onrender.com/films")
     .then(res => res.json())
@@ -57,7 +38,8 @@ function movieDetails(){
             const button = document.getElementById(`${movie.id}`)
             button.addEventListener('click', e =>{
                 let cards = document.getElementById('showing')
-            let remainingTicket = `${movie.capacity}` - `${movie.tickets_sold}`
+                
+                let remainingTicket = `${movie.capacity}` - `${movie.tickets_sold}`
             cards.innerHTML = `
             <div class= 'card'> 
             <div id= 'title' class = 'title'> ${movie.title} </div>
@@ -68,40 +50,74 @@ function movieDetails(){
                 <span id="showtime" class="ui label">${movie.showtime}</span>
                 <span id="ticket-num">${remainingTicket}</span> remaining tickets
             </div>
+            <br>
             <div class="extra content">
               <button id="buy-ticket" class="ui orange button">
                 Buy Ticket
               </button>
             </div>
             `  
-        const pic = document.getElementById('poster')   
-        pic.src =`${movie.poster}`   
+            const pic = document.getElementById('poster')   
+            pic.src =`${movie.poster}` 
+
+       
+                        
+             
+
+// Deducting ticket once it is bought
+        console.log( cards.querySelector('#buy-ticket').addEventListener('click', () => {
+            
+            remainingTicket -= 1  
+            cards.querySelector("#ticket-num").textContent = remainingTicket 
+            updateTickets(movie) // we'll use this as a function to update the remaining tickets using the patch method
+           
+        }))
+         cards.querySelector("#buy-ticket").removeEventListener('click', (remainingTicket < 0))
+             remainingTicket <= 0 
+                cards.querySelector("#ticket-num").textContent = remainingTicket
             })
         })
         })
-}
 
-// console.log( document.querySelector('#f-Items').addEventListener('mousover', forTitles))
 
-//function clickEvent
+       
+      
 
-/*
-const forPoster = function(){
-    fetch("http://localhost:3000/films/1")
-    .then(res => res.json())
-    .then((data) => {
-        console.log(data);
-        data.forEach((movie) => {
-            //const posterDiv = document.createElement('div')
-            //posterDiv.textContent = movie.poster
-            // titleFilms.appendChild(titleDiv)
+// PATCH METHOD => it is being used to update  the remaining tickets
 
-        })
+function updateTickets(ticket){
+    fetch( `https://json-server-1-wvrp.onrender.com/films/${ticket.id}`,{
+        method: 'PATCH',
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ticket)
     })
-  
+    .then(res => res.json())
+    .then(movie => console.log(movie))
 }
-forPoster()
 
-document.getElementById("f-Item").addEventListener('click', forPoster)
-*/
+// DELETE METHOD
+
+// function erase(){
+//     fetch( `https://json-server-1-wvrp.onrender.com/films/`)
+//     .then(res => res.json())
+//     .then(data => {
+//      data.forEach(movie => { 
+//      document.querySelector('#delete-btn').addEventListener('click', () => {
+//         cards.innerHTML = ''
+        
+//     })
+//     document.querySelector('#delete-btn').addEventListener('click', () => {
+//         pic.remove()
+//     })
+//     })
+//     })    }  
+//     erase()
+
+
+}
+
+
+
 
